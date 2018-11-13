b410bf0a0a57d3c77a07ee8bd53bf94962ad3c35
LehmanBrother <chrislehman1991@gmail.com>
2018-11-13 17:47:05 -0600

    Commit: Carmen Sandiego - Halfway there! Hot on her trail.

--- carmen-sandiego/find_carmen.sql
+++ carmen-sandiego/find_carmen.sql
@@ -3,6 +3,8 @@
 -- use two `-` to comment at the start of a line, or highlight the text and press `⌘/` to toggle comments
 -- EXAMPLE: SELECT ALL FROM THE TABLE COUNTRY AND LIMIT IT TO ONE ENTRY
 
+CREATE DATABASE carmen;
+
 SELECT * FROM COUNTRY LIMIT 1;
 
 --  -[ RECORD 1 ]--+--------------------------
@@ -24,21 +26,38 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
-
+SELECT name
+FROM country
+WHERE region = 'Southern Europe'
+ORDER BY population ASC;
+--least populous country: Holy See (Vatican City State)
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
-
-
+SELECT language
+FROM countrylanguage
+WHERE countrycode = 'VAT';
+--Italian
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
-
+SELECT
+	co.name
+FROM countrylanguage cl
+JOIN country co ON cl.countrycode = co.code
+WHERE cl.language = 'Italian'
+	AND cl.percentage = 100;
+--San Marino
 
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
-
+SELECT
+	name
+FROM city
+WHERE countrycode = 'SMR'
+	AND name <> 'San Marino';
+--Serravalle
 
 
 
