585d5289850d0289b9892c19e41202ca4dfff064
Charlotte Prevost <chaprevost91@gmail.com>
2018-11-14 09:06:28 -0600

    Commit: REALTY - Realty schema created

--- realty/realty.sql
+++ realty/realty.sql
@@ -6,6 +6,44 @@ CREATE DATABASE realty_db;
 ----------------------------------------------------------------------
 ----------------------------------------------------------------------
 
+DROP TABLE if exists apartments;
+DROP TABLE if exists offices;
+DROP TABLE if exists storefronts;
+
+CREATE TABLE apartments(
+	id SERIAL PRIMARY KEY,
+	apt SMALLINT,
+	bedrooms SMALLINT,
+	bathrooms SMALLINT,
+	address VARCHAR(255) NOT NULL
+	tenant VARCHAR(64),
+	occupied BOOLEAN,
+	square_feet NUMERIC,
+	price NUMERIC
+);
+
+CREATE TABLE offices(
+	id SERIAL PRIMARY KEY,
+	address VARCHAR(255) NOT NULL,
+	bathrooms SMALLINT,
+	kitchen BOOLEAN,
+	occupied BOOLEAN,
+	square_feet NUMERIC,
+	price NUMERIC,
+	company VARCHAR(128)
+);
+
+CREATE TABLE storefronts(
+	id SERIAL PRIMARY KEY,
+	address VARCHAR(255) NOT NULL,
+	occupied BOOLEAN,
+	price NUMERIC,
+	kitchen BOOLEAN,
+	square_feet NUMERIC,
+	owner VARCHAR(64),
+	outdoor_seating BOOLEAN DEFAULT FALSE
+);
+
 
 ----------------------------------------------------------------------
 ------------------------------ COMMIT 2 ------------------------------
