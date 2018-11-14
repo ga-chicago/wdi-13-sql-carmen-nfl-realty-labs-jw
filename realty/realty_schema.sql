725ea69b5e622f71b74410ea4b722fe8e1398e20
LehmanBrother <chrislehman1991@gmail.com>
2018-11-14 16:28:42 -0600

    Realty schema

--- realty/realty_schema.sql
+++ realty/realty_schema.sql
@@ -1,1 +1,26 @@
 -- enter your schema for apartments, offices and storefronts below
+drop table if exists apartments;
+drop table if exists storefronts;
+
+create table apartments(
+	id serial primary key,
+	apt_number integer,
+	bedrooms integer,
+	bathrooms integer,
+	address varchar(128) not null,
+	tenant varchar(128),
+	occupied boolean,
+	sq_ft numeric,
+	price numeric
+);
+
+create table storefronts(
+	id serial primary key,
+	address varchar(128),
+	occupied boolean,
+	price numeric,
+	kitchen boolean,
+	sq_ft numeric,
+	owner varchar(128),
+	outdoor_seating boolean default false
+);
