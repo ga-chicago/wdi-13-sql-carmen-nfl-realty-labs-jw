d4a99a903ea10ccfb526e50516bcfe221632c48d
Andy Prudhomme <aprudhomme9@gmail.com>
2018-11-14 14:33:30 -0600

    Found her

--- carmen-sandiego/find_carmen.sql
+++ carmen-sandiego/find_carmen.sql
@@ -55,13 +55,24 @@ SELECT * FROM city
 -- Serravalle
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT * FROM city
+	JOIN country ON country.code = city.countrycode
+	WHERE country.continent = 'South America' AND city.name LIKE 'Serra';
 
+-- Serra
 
+SELECT country.name, country.code, country.capital FROM country
+	JOIN city ON city.countrycode = country.code
+	WHERE city.name = 'Serra';
+
+-- Brazil
+-- Capital = 211
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
+SELECT * FROM city
+	WHERE city.id = 211;
 
-
-
+-- Brasilia
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
 
@@ -77,7 +88,8 @@ SELECT * FROM city
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+--population 91,084
+SELECT city.name FROM city
+	WHERE city.population = 91084;
 
-
-
--- She's in ____________________________!
+-- She's in _____Santa Monica_______________________!
